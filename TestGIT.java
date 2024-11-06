fa16e19859ae03e6ccb7ebf82de51910a436a5ea
gayashan <gayashan.jayawardena@gmail.com>
2024-11-07 02:44:29 +0530

    American guy commit

---
+++ TestGIT.java
@@ -0,0 +1,12 @@
+
+public class TestGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world 1");
+		System.out.println("Hello world 2");
+
+	}
+
+}
