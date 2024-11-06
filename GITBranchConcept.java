4bd4592c5c7c5c0fcf903c926c032e7d95754af3
gayashan <gayashan.jayawardena@gmail.com>
2024-11-07 03:12:30 +0530

    Architect Asian deevelopment

---
+++ GITBranchConcept.java
@@ -0,0 +1,12 @@
+
+public class GITBranchConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("GIT branch concept 1");
+		System.out.println("GIT branch concept 2");
+
+	}
+
+}
--- TestGIT.java
+++ TestGIT.java
@@ -6,6 +6,9 @@ public class TestGIT {
 		
 		System.out.println("Hello world 1");
 		System.out.println("Hello world 2");
+		
+		System.out.println("Hello world 3 in Develop branch");
+		System.out.println("Hello world 4 in Develop branch");
 
 	}
 
